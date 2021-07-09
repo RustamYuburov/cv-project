7d5865386282239c3b4c0affc1ff56638842dc7a
Rustam Yuburov <rt138328@gmail.com>
2021-07-09 22:13:26 +0600

    Changed component to stateless

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,23 +5,7 @@ import GeneralInfoForm from './GeneralInfo/GeneralInfoForm'
 class MainContent extends Component {
    constructor(props) {
      super(props)
-     this.state = {
-      firstName: '',
-      lastName: '',
-      occupation: '',
-      address: '',
-      phone: '',
-      email: ''
-    }
    }
-
-   
-  handlePersonalChange = (field, e) => {
-    this.setState(prevState => {
-      [field] = e.target.value
-    })
-    console.log(this.state.generalInfo)
-  }
 
    render() {
 
