715988a3cc6b0db0b402de6484c063269ebd9bfb
Rustam Yuburov <rt138328@gmail.com>
2021-07-09 22:12:41 +0600

    Added more GeneralInfo component

--- src/components/GeneralInfo/GeneralInfo.js
+++ src/components/GeneralInfo/GeneralInfo.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
-
+import GeneralInfoForm from './GeneralInfoForm';
+import GeneralInfoView from './GeneralInfoView';
 class GeneralInfo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      occupation: '',
+      address: '',
+      phone: '',
+      email: ''
+    }
   }
+
+  handlePersonalChange = (field, e) => {
+    this.setState(prevState => {
+      [field] = e.target.value
+    })
+    console.log(this.state.generalInfo)
+  }
+
 
   render() {
     return(
---
+++ src/components/GeneralInfo/GeneralInfoView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class GeneralInfoView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return(
+      <div>
+        Hello
+      </div>
+    ) 
+  }
+}
+
+export default GeneralInfoView
