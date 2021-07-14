09898615fb359b5b135a0a6f7fb60fb5f92bb440
Rustam Yuburov <rt138328@gmail.com>
2021-07-14 23:20:35 +0600

    Added skills component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import GeneralInfo from './GeneralInfo/GeneralInfo';
 import EducationInfo from './EducationInfo/EducationInfo';
+// import WorkExperience from './WorkExperience/WorkExperience';
+import Skills from './Skills/Skills';
 
-const MainContent = (props) => {
+const MainContent = () => {
 
      return(
        <div className='mainContent'>
          <div className='generalInfo'>
+           <h1>General Information</h1>
           <GeneralInfo />
          </div>
          <hr/>
          <div className='educationInfo'>
+           <h1>Education</h1>
            <EducationInfo />
          </div>
          <hr/>
          <div className='workExperienceInfo'>
+           <h1>Work Experience</h1>
            {/* <WorkExperienceInfo /> */}
          </div>
          <hr/>
          <div className='skills'>
-           {/* <Skills /> */}
+         <h1>Skills</h1>
+           <Skills />
          </div>
        </div>
      )
