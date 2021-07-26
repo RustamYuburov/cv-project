bfd31777f4fbddb19d51e1bbb3e8892c5f363bd6
Rustam Yuburov <rt138328@gmail.com>
2021-07-26 21:51:34 +0600

    Reworked code to use hooks

--- src/components/EducationInfo/EducationInfo.js
+++ src/components/EducationInfo/EducationInfo.js
@@ -1,136 +1,106 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import uniqid from 'uniqid';
 import '../../styles/EducationInfoStyle.css';
 import EducationInfoView from './EducationInfoView';
 import EducationInfoForm from './EducationInfoForm';
 
-class EducationInfo extends Component {
-  constructor(props) {
-    super(props);
+const EducationInfo = () => {
+  const [educations, setEducations] = useState([]);
+  const [educationForms, setEducationForms] = useState([]);
 
-    this.state = {
-      educations: [],
-      educationForms: [],
-    };
-  }
-
-  handleEduInput = (e, id) => {
-    this.setState((prevState) => {
-      const { name, value } = e.target;
-      const updatedEduForm = prevState.educationForms.map((forms) => {
-        if (forms.id === id) {
-          return {
-            ...forms,
-            [name]: value,
-          };
-        }
-        return forms;
-      });
-
-      return {
-        ...this.state,
-        educationForms: updatedEduForm,
-      };
+  const handleEduInput = (e, id) => {
+    const { name, value } = e.target;
+    const updatedEduForm = educationForms.map((form) => {
+      if (form.id === id) {
+        return {
+          ...form,
+          [name]: value,
+        };
+      }
+      return form;
     });
+    setEducationForms(updatedEduForm);
   };
 
-  saveEducation = (e, id) => {
-    e.preventDefault();
-    this.setState((prevState) => {
-      const education = prevState.educationForms.filter(
-        (form) => form.id === id
-      );
-      return {
-        educations: prevState.educations.concat(education),
-        educationForms: prevState.educationForms.filter(
-          (educationForms) => educationForms.id !== id
-        ),
-      };
-    });
-  };
-
-  addEducation = () => {
-    this.setState({
-      educationForms: this.state.educationForms.concat({
+  const addEducation = () => {
+    setEducationForms(
+      educationForms.concat({
         school: '',
         degree: '',
         subject: '',
         startDate: '',
         endDate: '',
         id: uniqid(),
-      }),
-    });
-  };
-
-  cancelEducation = (e) => {
-    this.setState((prevState) => {
-      const updateEduForm = prevState.educationForms.filter(
-        (education) => education.id !== e.target.name
-      );
-      return {
-        ...prevState,
-        educationForms: updateEduForm,
-      };
-    });
-  };
-
-  editEducation = (e) => {
-    e.preventDefault();
-    this.setState((prevState) => {
-      const eduInfo = prevState.educations.filter(
-        (education) => education.id === e.target.name
-      );
-      return {
-        educationForms: prevState.educationForms.concat(eduInfo),
-        educations: prevState.educations.filter(
-          (education) => education.id !== e.target.name
-        ),
-      };
-    });
-  };
-
-  deleteEducation = (e) => {
-    e.preventDefault();
-    this.setState((prevState) => {
-      const updatedEdu = prevState.educations.filter(
-        (education) => education.id !== e.target.name
-      );
-      return {
-        ...prevState,
-        educations: updatedEdu,
-      };
-    });
-  };
-
-  render() {
-    const educationViews = this.state.educations.map((education) => (
-      <EducationInfoView
-        handleDelete={this.deleteEducation}
-        handleEdit={this.editEducation}
-        info={education}
-        key={education.id}
-      />
-    ));
-    const educationForms = this.state.educationForms.map((educationForm) => (
-      <EducationInfoForm
-        saveEdu={this.saveEducation}
-        handleCancel={this.cancelEducation}
-        handleEduInput={this.handleEduInput}
-        info={educationForm}
-        key={educationForm.id}
-      />
-    ));
-
-    return (
-      <div className="educationInfo">
-        <div className="educationInfoViewDiv">{educationViews}</div>
-        <div className="educationInfoFormDiv">{educationForms}</div>
-        <button onClick={this.addEducation} className="buttons addBtns">
-          Add More
-        </button>
-      </div>
+      })
     );
-  }
-}
+  };
+
+  const saveEducation = (e, id) => {
+    e.preventDefault();
+    const education = educationForms.filter((form) => form.id === id);
+    setEducations(educations.concat(education));
+
+    const updatedEduForm = educationForms.filter(
+      (educationForms) => educationForms.id !== id
+    );
+    setEducationForms(updatedEduForm);
+  };
+
+  const cancelEducation = (e) => {
+    const updateEduForm = educationForms.filter(
+      (education) => education.id !== e.target.name
+    );
+    setEducationForms(updateEduForm);
+  };
+
+  const editEducation = (e) => {
+    e.preventDefault();
+    const eduInfo = educations.filter(
+      (education) => education.id === e.target.name
+    );
+    setEducationForms(educationForms.concat(eduInfo));
+
+    const updatedEdu = educations.filter(
+      (education) => education.id !== e.target.name
+    );
+    setEducations(updatedEdu);
+  };
+
+  const deleteEducation = (e) => {
+    e.preventDefault();
+    const updatedEdu = educations.filter(
+      (education) => education.id !== e.target.name
+    );
+    setEducations(updatedEdu);
+  };
+
+  const educationViews = educations.map((education) => (
+    <EducationInfoView
+      handleDelete={deleteEducation}
+      handleEdit={editEducation}
+      info={education}
+      key={education.id}
+    />
+  ));
+  const educationFormsView = educationForms.map((educationForm) => (
+    <EducationInfoForm
+      saveEdu={saveEducation}
+      handleCancel={cancelEducation}
+      handleEduInput={handleEduInput}
+      info={educationForm}
+      key={educationForm.id}
+    />
+  ));
+
+  return (
+    <div className="educationInfo">
+      <div className="educationInfoViewDiv">{educationViews}</div>
+      <div className="educationInfoFormDiv">{educationFormsView}</div>
+      <button onClick={addEducation} className="buttons addBtns">
+        Add More
+      </button>
+    </div>
+  );
+};
 
 export default EducationInfo;
